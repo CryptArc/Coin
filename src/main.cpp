63475be85c6affd7cd30e8d9e2487debf8af5c9d
PlatinumCrypto <info@plt.co.in>
2014-04-04 21:21:55 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1253,15 +1253,15 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = nStartSubsidy;
 
     // Mining phase: Subsidy is cut in half every SubsidyHalvingInterval
-   // nSubsidy >>= (nHeight / Params().SubsidyHalvingInterval());
+    nSubsidy >>= (nHeight / Params().SubsidyHalvingInterval());
     
     // Inflation phase: Subsidy reaches minimum subsidy
     // Network is rewarded for transaction processing with transaction fees and 
     // the inflationary subsidy
-   // if (nSubsidy < nMinSubsidy)
-   // {
-        //nSubsidy = nMinSubsidy;
-   // }
+    if (nSubsidy < nMinSubsidy)
+    {
+        nSubsidy = nMinSubsidy;
+    }
 
     return nSubsidy + nFees;
 }
